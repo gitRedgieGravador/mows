1469e58146f8342dfb2e339bb71755ba8bcb07b3
Redgie Gravador <54094125+gitRedgieGravador@users.noreply.github.com>
2019-08-28 20:44:00 +0800

    Update controller.js

--- scripts/controller.js
+++ scripts/controller.js
@@ -1,26 +1,23 @@
 $(document).ready(function () {
 	var rownum = 0;
-	$("#btnConnect").click(function(){
+	$("#btnConnect").click(function () {
 		let address = $("#brokerAddress").val()
 		client = mqtt.connect(address)
-		
+
 		$("#status").val("Connecting.....")
 
-		client.on("connect", function(){
+		client.on("connect", function () {
 			$("#status").val("Successfully connected!");
-		    console.log("Successfully connected");
 		})
 
 		client.on("message", function (topic, payload) {
-		  	console.log([topic, payload].join(": "));
-		  	let getTopic = topic.toString().slice(7);
-		  	//console.log("lalal :" + getTopic.slice(7));
-		  	var stamp = new Date($.now());
-		  	let parent = $("#tbodyContainer");
+			let getTopic = topic.toString().slice(7);
+			var stamp = new Date($.now());
+			let parent = $("#tbodyContainer");
 			let row = $("<tr></tr>");
 			let top = $("<th></th>").text(getTopic);
 			let payld = $("<td></td>").text(payload);
-			let time = $("<td></td>").text(stamp.toString().slice(0,24));
+			let time = $("<td></td>").text(stamp.toString().slice(0, 24));
 
 			top.attr("scope", "row");
 			row.attr("id", rownum);
@@ -30,20 +27,28 @@ $(document).ready(function () {
 			rownum += 1;
 		})
 
-		
+
 	});
 
-	$("#btnSubscribe").click(function(){
+	$("#btnDisconnect").click(function () {
+		$("#status").val("Disconnected!")
+		client.end();
+	});
+
+	$("#btnSubscribe").click(function () {
 		let topicSubs = "redgie/" + $("#topicToSubscribe").val();
 		client.subscribe(topicSubs);
-		console.log("btnSubscribe is click " + topicSubs);
 	});
 
-	$("#btnPublish").click(function(){
+	$("#btnPublish").click(function () {
 		let top = "redgie/" + $("#topicToPublish").val();
 		let payld = $("#payloadToPublish").val();
 		client.publish(top, payld);
-		console.log("btnPublish is click " + top);
+	});
+
+	$("#btnUnsubscribe").click(function () {
+		let topicSubs = "redgie/" + $("#topicToSubscribe").val();
+		client.unsubscribe(topicSubs);
 	});
 });
 
